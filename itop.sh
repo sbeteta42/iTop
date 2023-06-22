1c9682c47523f4984b5957d480e50c5b97db04be
Noa <noa.ledet@thelightguardian.fr>
2023-06-22 10:38:41 +0200

    modified:   itop.sh

--- itop.sh
+++ itop.sh
@@ -23,13 +23,13 @@ wget https://sourceforge.net/projects/itop/files/latest/download
 
 mv download iTop-3.0.3-10998.zip
 
-unzip iTop-3.0.3-10998.zip /var/www/itop
+mkdir /var/www/itop
+unzip iTop-3.0.3-10998.zip /var/www/itop/
 
-# mv iTop-3.0.3-10998/* /var/www/itop/
+#mv iTop-3.0.3-10998/* /var/www/itop/
 
 echo "Modification des dossiers du serveur web..."
 
-mkdir /var/www/itop
 rm -r /var/www/html
 
 chown -R www-data:www-data /var/www/itop/
