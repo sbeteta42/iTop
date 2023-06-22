ccf600fa409af573f0754c8623cf2a66da642947
Noa <noa.ledet@thelightguardian.fr>
2023-06-22 10:03:27 +0200

    new file:   itop.sh

---
+++ itop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+ 
+echo "Installation des mises à jours et des dépendences..." 
+ 
+apt update -y && apt full-upgrade -y && apt-get install -y apache2 mariadb-server 
+ 
+apt-get install -y php php-mysql php-ldap php-cli php-soap php-json graphviz php-xml php-gd php-zip libapache2-mod-php php-mbstring unzip 
+ 
+echo "Installation terminé !"
+echo "Création de la base de donnée et de l'utilisateur..."
+
+mysql -u root <<EOF
+CREATE DATABASE itop;
+CREATE USER 'itop'@'localhost' IDENTIFIED BY 'operations';
+GRANT ALL PRIVILEGES ON itop.* TO 'itop'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+echo "Création de la base de donnée et de l'utilisateur terminé !"
+echo "Téléchargement de itop et installation..."
+
+wget https://sourceforge.net/projects/itop/files/latest/download
+
+mv download iTop-3.0.3-10998.zip
+
+unzip download iTop-3.0.3-10998.zip
+
