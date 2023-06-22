b4fff8da953aab945bb80a09d3c9884c6076c2b2
Noa <noa.ledet@thelightguardian.fr>
2023-06-22 10:55:23 +0200

    e

--- itop.sh
+++ itop.sh
@@ -2,9 +2,10 @@
  
 echo "Installation des mises à jours et des dépendences..." 
  
-apt update -y && apt full-upgrade -y && apt-get install -y apache2 mariadb-server 
+apt update -y && apt full-upgrade -y
+apt-get install -y apache2 mariadb-server 
  
-apt-get install -y php php-mysql php-ldap php-cli php-soap php-json graphviz php-xml php-gd php-zip libapache2-mod-php php-mbstring unzip 
+apt-get install -y php php-mysql php-ldap php-cli php-soap php-json php-curl graphviz php-xml php-gd php-zip libapache2-mod-php php-mbstring unzip 
  
 echo "Installation terminé !"
 echo "Création de la base de donnée et de l'utilisateur..."
@@ -26,8 +27,6 @@ mv download iTop-3.0.3-10998.zip
 mkdir /var/www/html/itop
 
 unzip iTop-3.0.3-10998.zip -d /var/www/html/itop/
-
-#mv iTop-3.0.3-10998/* /var/www/itop/
 
 echo "Modification des dossiers du serveur web..."
 
