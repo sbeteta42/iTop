d59994bf5e62ebf46a0c7b3a617320919b6212c3
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-01-21 15:55:42 +0100

    Update itop.sh

--- itop.sh
+++ itop.sh
@@ -4,14 +4,15 @@
 echo "Installation des mises à jours et des dépendences..." 
 
 # Met à jour le système avec apt, et installe les paquets apache2 et mariadb-server
+echo "On met à jour le système avec apt et on installe les paquets apache2 et mariadb-server"
 apt update -y && apt full-upgrade -y
 apt-get install -y apache2 mariadb-server 
+clear
 
 # Installe divers paquets PHP et d'autres dépendances nécessaires à iTop
+echo "On Installe divers paquets PHP et d'autres dépendances nécessaires à iTop"
 apt-get install -y php php-mysql php-ldap php-cli php-soap php-json php-curl graphviz php-xml php-gd php-zip libapache2-mod-php php-mbstring unzip 
-
-# Affiche un message pour indiquer que l'installation est terminée
-echo "Installation terminé !"
+clear
 
 # Affiche un message pour indiquer que la création de la base de données et de l'utilisateur va commencer
 echo "Création de la base de donnée et de l'utilisateur..."
@@ -28,7 +29,7 @@ EOF
 echo "Création de la base de donnée et de l'utilisateur terminé !"
 
 # Affiche un message pour indiquer que le téléchargement et l'installation de iTop vont commencer
-echo "Téléchargement de iTop et installation..."
+echo "Téléchargement de iTop et installation...Please wait !"
 
 # Télécharge la dernière version de iTop depuis SourceForge
 wget https://sourceforge.net/projects/itop/files/latest/download
@@ -41,6 +42,7 @@ mkdir /var/www/html/itop
 
 # Décompresse l'archive iTop dans le dossier du serveur web
 unzip iTop-3.0.3-10998.zip -d /var/www/html/itop/
+clear
 
 # Affiche un message pour indiquer que les dossiers du serveur web vont être modifiés
 echo "Modification des dossiers du serveur web..."
@@ -57,8 +59,11 @@ echo "Démarrage du serveur apache..."
 # Démarre le service Apache
 service apache2 start
 systemctl enable apache2
-
-# Affiche un message pour indiquer que le serveur à démarrer
+# Affiche un message pour indiquer que le serveur à démarre
 
 echo "Le serveur apache a été démarré !"
+# Affiche un message pour indiquer que l'installation est terminée
+echo "Installation de iTop terminé !"
+
 echo "Note : Pour accéder à l'interface web, vous devez saisir ceci : http://@ip_serveur/itop/web"
+
