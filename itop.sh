596fb8c88dcbb9e90f7b553852ab46213a9856e6
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-04-19 11:17:55 +0200

    Update itop.sh

--- itop.sh
+++ itop.sh
@@ -2,21 +2,24 @@
  
 # Affiche un message pour indiquer le début de l'installation
 echo "Installation des mises à jours et des dépendences..." 
+sleep 3
 
 # Met à jour le système avec apt, et installe les paquets apache2 et mariadb-server
 echo "On met à jour le système avec apt et on installe les paquets apache2 et mariadb-server"
+sleep 3
 apt update -y && apt full-upgrade -y
 apt-get install -y apache2 mariadb-server 
 clear
 
 # Installe divers paquets PHP et d'autres dépendances nécessaires à iTop
 echo "On Installe divers paquets PHP et d'autres dépendances nécessaires à iTop"
+sleep 3
 apt-get install -y php php-mysql php-ldap php-cli php-soap php-json php-curl graphviz php-xml php-gd php-zip libapache2-mod-php php-mbstring unzip 
 clear
 
 # Affiche un message pour indiquer que la création de la base de données et de l'utilisateur va commencer
 echo "Création de la base de donnée et de l'utilisateur..."
-
+sleep 3
 # Crée une nouvelle base de données et un nouvel utilisateur dans MariaDB
 mysql -u root <<EOF
 CREATE DATABASE iTop;
@@ -27,10 +30,10 @@ EOF
 
 # Affiche un message pour indiquer que la création de la base de données et de l'utilisateur est terminée
 echo "Création de la base de donnée et de l'utilisateur terminé !"
-
+sleep 2
 # Affiche un message pour indiquer que le téléchargement et l'installation de iTop vont commencer
 echo "Téléchargement de iTop et installation...Please wait !"
-
+sleep 5
 # Télécharge la dernière version de iTop depuis SourceForge
 wget https://sourceforge.net/projects/itop/files/latest/download
 
@@ -46,7 +49,7 @@ clear
 
 # Affiche un message pour indiquer que les dossiers du serveur web vont être modifiés
 echo "Modification des dossiers du serveur web..."
-
+sleep 3
 # Change le propriétaire des fichiers iTop à www-data (l'utilisateur par défaut du serveur web Apache sur Debian)
 chown -R www-data:www-data /var/www/html/itop/
 
@@ -55,7 +58,7 @@ chmod -R 744 /var/www/html/itop/
 
 # Affiche un message pour indiquer que le serveur va démarrer
 echo "Démarrage du serveur apache..."
-
+sleep 2
 # Démarre le service Apache
 service apache2 start
 systemctl enable apache2
@@ -64,6 +67,6 @@ systemctl enable apache2
 echo "Le serveur apache a été démarré !"
 # Affiche un message pour indiquer que l'installation est terminée
 echo "Installation de iTop terminé !"
-
+echo .
+echo ..
 echo "Note : Pour accéder à l'interface web, vous devez saisir ceci : http://@ip_serveur/itop/web"
-
